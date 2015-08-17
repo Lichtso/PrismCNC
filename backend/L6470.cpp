f743ad3a013aed8ce77abb980273ff179f1f7ad8
Alexander Meißner <AlexanderMeissner@gmx.net>
2015-08-17 17:28:59 +0200

    Fixed SPI message length

--- backend/L6470.cpp
+++ backend/L6470.cpp
@@ -8,19 +8,19 @@ bool L6470::set(uint8_t len, uint8_t key, uint32_t value) {
     buffer[0] = key;
     buffer[1] = buffer[2] = buffer[3] = 0x00;
     for(size_t i = 1; i <= len; ++i)
-        buffer[i] = value >> (8*(len-i));
-    return bus->transfer(slaveIndex, buffer, 1+len);
+        buffer[i] = value >> (8*(3-i));
+    return bus->transfer(slaveIndex, buffer, (len == 1) ? 1 : 4);
 }
 
 bool L6470::get(uint8_t len, uint8_t key, uint32_t& value) {
     uint8_t buffer[4];
     buffer[0] = key;
     buffer[1] = buffer[2] = buffer[3] = 0x00;
-    if(!bus->transfer(slaveIndex, buffer, 1+len))
+    if(!bus->transfer(slaveIndex, buffer, 4))
         return false;
     value = 0;
     for(size_t i = 1; i <= len; ++i)
-        value |= buffer[i] << (8*(len-i));
+        value |= buffer[i] << (8*(3-i));
     return true;
 }
 
